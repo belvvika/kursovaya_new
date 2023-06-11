60f427f8fb6cc4ca9273d44e9ec964527f4bd0ed
belvvika <bellvika132@gmail.com>
2023-06-12 01:54:55 +0300

    test sort data

--- package-name/tests/test_functions.py
+++ package-name/tests/test_functions.py
@@ -1,7 +1,11 @@
-
-from package_name.functions import read_json
+from package_name.functions import read_json, sort_data
 
 
 
 def test_read_json(path_test_json,expected_result):
     assert read_json(path_test_json) == expected_result
+
+
+
+def test_sort_data(data_for_sort, expected_result_for_sort):
+    assert sort_data(data_for_sort) == expected_result_for_sort
