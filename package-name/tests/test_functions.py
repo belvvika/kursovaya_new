a087ec73339638124a0a2ca362d1792870e8be19
belvvika <bellvika132@gmail.com>
2023-06-12 11:27:48 +0300

    test hide card

--- package-name/tests/test_functions.py
+++ package-name/tests/test_functions.py
@@ -1,4 +1,4 @@
-from package_name.functions import read_json, sort_data
+from package_name.functions import read_json, sort_data, hide_card
 
 
 
@@ -9,3 +9,7 @@ def test_read_json(path_test_json,expected_result):
 
 def test_sort_data(data_for_sort, expected_result_for_sort):
     assert sort_data(data_for_sort) == expected_result_for_sort
+
+
+def test_hide_card(expected_result_for_hide_card):
+    assert hide_card(expected_result_for_hide_card)
