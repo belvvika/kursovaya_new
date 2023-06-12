3ac5f1268ee080bc9ce532fc2d9697937f15d9c5
belvvika <bellvika132@gmail.com>
2023-06-12 12:37:30 +0300

    add expexted_result_description

--- package-name/tests/conftest.py
+++ package-name/tests/conftest.py
@@ -207,3 +207,21 @@ def expexted_result_data():
         "from": "Maestro 1596837868705199",
         "to": "Счет 64686473678894779589"
     }]
+
+@pytest.fixture
+def expexted_result_description():
+    return [{
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }]
