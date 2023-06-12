3d7b4f9e52350270fe5838d9e143c56981b5115a
belvvika <bellvika132@gmail.com>
2023-06-12 11:53:15 +0300

    add expected_result_for_hide_amount

--- package-name/tests/conftest.py
+++ package-name/tests/conftest.py
@@ -170,3 +170,22 @@ def expected_result_for_hide_card():
         "from": "Maestro 1596837868705199",
         "to": "Счет 64686473678894779589"
     }]
+
+
+@pytest.fixture
+def expexted_result_for_hide_amount():
+    return [{
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }]
