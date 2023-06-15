cbc584f1a69c0523ab02f170e18d8cce430fbc53
belvvika <bellvika132@gmail.com>
2023-06-15 18:42:55 +0300

    add executed_operation_list

--- package-name/src/main.py
+++ package-name/src/main.py
@@ -1,3 +1,21 @@
-from functions import read_json, sort_data, hide_card, hide_amount, data, description, amount
+from functions import read_json
 import os
-read_json(os.path.join(os.path.dirname(__file__), 'operations.json'))
+read_json_list = read_json(os.path.join(os.path.dirname(__file__), 'operations.json'))
+for key in read_json_list:
+    key_sort_data = key.get('state')
+    key_hide_card = key.get('from')
+    key_hide_amount = key.get('to')
+    key_data = key.get('date')
+    key_description = key.get('description')
+    key_amount = key.get('operationAmount')
+    key_code = key.get('operationAmount')
+    if key_sort_data == 'EXECUTED':
+        executed_operation_list = []
+        executed_operation_list.append(key_data[0:10])
+        executed_operation_list.append(key_description)
+        executed_operation_list.append(key_hide_card)
+        executed_operation_list.append(key_hide_amount)
+        executed_operation_list.append(key_amount['amount'])
+        executed_operation_list.append(key_code['currency'])
+for keys in executed_operation_list:
+    print(keys)
--- package-name/tests/test_functions.py
+++ package-name/tests/test_functions.py
@@ -5,11 +5,8 @@ from src.functions import read_json, sort_data, hide_card, hide_amount, data, de
 def test_read_json(path_test_json, expected_result):
     assert read_json(path_test_json) == expected_result
 
-
-
 def test_sort_data(data_for_sort, expected_result_for_sort):
     assert sort_data(data_for_sort) == expected_result_for_sort
-
 
 def test_hide_card(expected_result_for_hide_card):
     assert hide_card(expected_result_for_hide_card)
