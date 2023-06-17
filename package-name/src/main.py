795095eeb4861b56c3e0fc06fa35113f2d13b79d
belvvika <bellvika132@gmail.com>
2023-06-17 15:43:31 +0300

    add function_list_all

--- package-name/src/main.py
+++ package-name/src/main.py
@@ -1,21 +1,15 @@
-from functions import read_json
+from functions import read_json, sort_data, hide_card, sort_data_by_date, hide_amount, amount, description
 import os
 read_json_list = read_json(os.path.join(os.path.dirname(__file__), 'operations.json'))
-for key in read_json_list:
-    key_sort_data = key.get('state')
-    key_hide_card = key.get('from')
-    key_hide_amount = key.get('to')
-    key_data = key.get('date')
-    key_description = key.get('description')
-    key_amount = key.get('operationAmount')
-    key_code = key.get('operationAmount')
-    if key_sort_data == 'EXECUTED':
-        executed_operation_list = []
-        executed_operation_list.append(key_data[0:10])
-        executed_operation_list.append(key_description)
-        executed_operation_list.append(key_hide_card)
-        executed_operation_list.append(key_hide_amount)
-        executed_operation_list.append(key_amount['amount'])
-        executed_operation_list.append(key_code['currency'])
-for keys in executed_operation_list:
-    print(keys)
+sort_data_list = sort_data(read_json_list)
+sort_data_by_date_list = sort_data_by_date(sort_data_list)
+hide_card_list = hide_card(sort_data_by_date_list)
+hide_amount_list = hide_amount(hide_card_list)
+amount_list = amount(hide_amount_list)
+description_list = description(amount_list)
+function_list_all = description_list[:5]
+
+for key in function_list_all:
+    print(f'{key.get("date")[:10]} -> {key.get("description")}')
+    print(f'{key.get("from")} -> {key.get("to")}')
+    print(f'{key.get("operationAmount")["amount"]} {key.get("operationAmount")["currency"]["name"]}')
